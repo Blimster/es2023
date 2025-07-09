ec75a3e387c9a378562cc1f649d1284a27dfe26d
Oliver Damm <oliver.damm@gmx.de>
2025-07-09 22:55:12 +0200

    Initial commit

---
+++ bin/es2023.dart
@@ -0,0 +1,6 @@
+import 'package:es2023/es2023.dart' as es2023;
+
+void main(List<String> arguments) {
+  print('Hello world: ${es2023.calculate()}!');
+  print('warp');
+}
---
+++ test/es2023_test.dart
@@ -0,0 +1,8 @@
+import 'package:es2023/es2023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
