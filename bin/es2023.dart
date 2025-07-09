b8516d370b2bf2b14e49277ac51bba90320ef768
Oliver Damm <oliver.damm@gmx.de>
2025-07-09 23:25:12 +0200

    replace project template files by real sources

--- bin/es2023.dart
+++
@@ -1,6 +0,0 @@
-import 'package:es2023/es2023.dart' as es2023;
-
-void main(List<String> arguments) {
-  print('Hello world: ${es2023.calculate()}!');
-  print('warp');
-}
--- test/es2023_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:es2023/es2023.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
